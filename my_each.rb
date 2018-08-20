8607b42b166e1014a15c543842e54ac676ab8717
tylerselldesign@gmail.com <tylerselldesign@gmail.com>
2018-08-20 21:48:34 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,9 @@
+collection = [0, 1, 2, 3]
 def my_each(collection)
   counter = 0
-  while counter < 10 do |word|
+  while counter < 2 do |word|
     word = collection(counter)
     counter += 1
   end
+  yield word
 end
